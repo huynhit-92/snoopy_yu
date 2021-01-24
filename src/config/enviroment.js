6f44f8d9c83a0dade207193a524475d7393dd5f1
Huynh <h-pham@starx.co.jp>
2021-01-24 19:36:29 +0900

    add

--- src/config/enviroment.js
+++ src/config/enviroment.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const API_KEY = "AIzaSyAlfy4z3ic61tExYEvrZNUJiaPMoq1DkFs"
 export const HOST = "https://youtube.googleapis.com/youtube/v3/"
-export const CAPTION_HOST = "http://video.google.com/timedtext?"
+export const CAPTION_HOST = "https://video.google.com/timedtext?"
 export function fetch(path) {
   return axios.get(HOST + path + `key=${API_KEY}`)
 }
